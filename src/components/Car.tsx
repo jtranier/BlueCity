164b4dd5b58693a97277c5c027cd9ed0e88787e7
Mathieu MAST <mmast@localhost.localdomain>
2020-03-02 11:17:28 +0100

    Draw car

--- src/components/Car.tsx
+++ src/components/Car.tsx
@@ -6,31 +6,46 @@ export const Car = (props: { x: number; y: number; width: number; height: number
     <React.Fragment>
       <Line
         points={[
-          props.x, props.y - .18 * props.height,
-          props.x - props.width, props.y - .18 * props.height,
-          props.x - props.width, props.y - props.height,
-          props.x - .5 * props.width, props.y - props.height,
-          props.x, props.y - .6 * props.height
+          props.x,
+          props.y - 0.18 * props.height,
+          props.x - props.width,
+          props.y - 0.18 * props.height,
+          props.x - props.width,
+          props.y - props.height,
+          props.x - 0.5 * props.width,
+          props.y - props.height,
+          props.x,
+          props.y - 0.6 * props.height
         ]}
         closed
         fill={'#44C'}
       />
       <Line
         points={[
-          props.x - .05 * props.width, props.y - .6 * props.height,
-          props.x - .7 * props.width, props.y - .6 * props.height,
-          props.x - .7 * props.width, props.y - .95 * props.height,
-          props.x - .45 * props.width, props.y - .95 * props.height
+          props.x - 0.05 * props.width,
+          props.y - 0.6 * props.height,
+          props.x - 0.7 * props.width,
+          props.y - 0.6 * props.height,
+          props.x - 0.7 * props.width,
+          props.y - 0.95 * props.height,
+          props.x - 0.45 * props.width,
+          props.y - 0.95 * props.height
         ]}
         closed
         fill={'#BBF'}
       />
       <Rect
-        x={props.x - .25 * props.width} y={props.y - .2 * props.height} width={.2 * props.width} height={.2 * props.height}
+        x={props.x - 0.25 * props.width}
+        y={props.y - 0.2 * props.height}
+        width={0.2 * props.width}
+        height={0.2 * props.height}
         fill={'#333'}
       />
       <Rect
-        x={props.x - .95 * props.width} y={props.y - .2 * props.height} width={.2 * props.width} height={.2 * props.height}
+        x={props.x - 0.95 * props.width}
+        y={props.y - 0.2 * props.height}
+        width={0.2 * props.width}
+        height={0.2 * props.height}
         fill={'#333'}
       />
     </React.Fragment>
