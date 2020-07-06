784c26add359125326b20251bc2f24482bde00f7
John Tranier <john.tranier@irit.fr>
2020-07-06 16:38:41 +0200

    Density graph (grid + axis + radar & traffic light)

--- src/components/Density.tsx
+++ src/components/Density.tsx
@@ -1,16 +1,39 @@
 import * as React from 'react';
-import { Rect, Text } from 'react-konva';
-import { useEngine } from '../hooks/useEngine';
-import { useConfig } from '../hooks/useConfig';
-import { useData } from '../hooks/useData';
-import { TrafficLight } from './TrafficLight';
-import { RectBtn } from './RectBtn';
-import { EditableNumber } from './EditableNumber';
+import {Line, Rect} from 'react-konva';
+import {useEngine} from '../hooks/useEngine';
+import {useConfig} from '../hooks/useConfig';
+import {useData} from '../hooks/useData';
+import * as _ from 'underscore';
+
 
 export const Density = (props: { x: number; y: number }) => {
   const engine = useEngine();
   const config = useConfig();
   const data = useData();
+
+  const grid: React.ReactFragment[] = [];
+
+  _.times(5, (i) =>
+    grid.push(
+      <Rect key={'line-horiz-' + i}
+            x={props.x}
+            y={props.y + (config.densityOrdinate / config.densityOrdinateResolution) / 5 * (i + 1)}
+            width={config.routeLen / config.densityResolution}
+            height={1}
+            fill="green"/>
+    )
+  )
+
+  _.times(9, (i) =>
+    grid.push(
+      <Rect key={'line-vert-' + i}
+            x={props.x + (config.routeLen / config.densityResolution) / 9 * (i+0.5)}
+            y={props.y}
+            width={1}
+            height={(config.densityOrdinate + 0.5) / config.densityOrdinateResolution}
+            fill="green"/>
+    )
+  )
 
   return (
     <React.Fragment>
@@ -21,6 +44,36 @@ export const Density = (props: { x: number; y: number }) => {
         height={(config.densityOrdinate + 0.5) / config.densityOrdinateResolution}
         fill="black"
       />
+      {grid}
+
+      {/* Radar */}
+      <Rect
+        x={props.x + (data.radar.pos) / config.densityResolution}
+        y={props.y + 3 / config.densityOrdinateResolution}
+        width={3}
+        height={(config.densityOrdinate - 3) / config.densityOrdinateResolution}
+        fill="red"
+      />
+
+      {/* Traffic light */}
+      {/* TODO : the traffic light pos should be a param */}
+      <Rect
+        x={props.x + 280 / config.densityResolution}
+        y={props.y}
+        width={3}
+        height={(config.densityOrdinate + 0.5) / config.densityOrdinateResolution}
+        fill="green"
+      />
+
+      {/* Density origin */}
+      <Rect
+        x={props.x}
+        y={props.y + 5 / config.densityOrdinateResolution}
+        width={config.routeLen / config.densityResolution}
+        height={3}
+        fill="green"
+      />
+
     </React.Fragment>
   );
 };
