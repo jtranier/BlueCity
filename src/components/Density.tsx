f55100b87ef65a1a35d01d13a4d3f11084cfaff4
John Tranier <john.tranier@irit.fr>
2020-10-07 14:11:14 +0200

    Fix density for the last visible car when traffic light is red

--- src/components/Density.tsx
+++ src/components/Density.tsx
@@ -79,7 +79,7 @@ export const Density = (props: { x: number; y: number }) => {
     }),
     (car) => {
       // Distance to next car
-      let dNextCar = car.precedingCar ? car.precedingCar.pos - car.pos : Infinity;
+      let dNextCar = car.precedingCar ? car.precedingCar.pos - car.pos : config.addCarDist;
       if (dNextCar < 0) {
         // TODO It should not happen, no?
         dNextCar = 0;
